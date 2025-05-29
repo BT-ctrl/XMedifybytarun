77dba02a8bf809c852551f8d9e4f1a15cc25852f
BT-ctrl <tripathitarun1620000@gmail.com>
2025-05-29 20:49:53 +0530

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css"; 
-import App from "./App";
-import Search from "./Search/Search";
-import Home from "./Home/Home";
-import MyBookings from "./MyBookings/MyBookings";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles.css";
 
+import App from "./App";
+import Home from "./Home/Home";
+import Search from "./Search/Search";
+import MyBookings from "./MyBookings/MyBookings";
 
 import { ThemeProvider, createTheme } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const theme = createTheme();
 
-const theme = createTheme(); 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "search", element: <Search /> },
-      { path: "my-bookings", element: <MyBookings /> },
-      { path: "/", element: <Home /> },
-    ],
-  },
+      {
+        index: true,         
+        element: <Home />
+      },
+      {
+        path: "search",
+        element: <Search />
+      },
+      {
+        path: "my-bookings",  
+        element: <MyBookings />
+      }
+    ]
+  }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
